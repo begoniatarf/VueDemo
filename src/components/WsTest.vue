d8e95292f216d4ce0778447e24c0614a17919377
haoc <haoc@rihuisoft.com>
2020-07-03 13:07:07 +0800

    element-ui

--- src/components/WsTest.vue
+++ src/components/WsTest.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="ws">
+    <el-row>
+      <el-col :span="12">
+        <div class="safeBox">
+          <el-card class="box-card">
+            <div v-for="msg in messageListA" :key="msg" class="text item">
+              {{'列表内容 ' + msg }}
+            </div>
+          </el-card>
+          <el-input
+            placeholder="请输入内容"
+            v-model="inputA"
+            clearable>
+          </el-input>
+        </div>
+      </el-col>
+      <el-col :span="12">
 
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -9,7 +27,10 @@ export default {
   name: 'WsTest',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      inputA: '',
+      inputB: '',
+      messageListA: ['1', '2'],
+      messageListB: []
     }
   }
 }
@@ -17,4 +38,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .safeBox {
+    width: 64%;
+    height: 720px;
+    border: 1px #ccc solid;
+    border-radius: 6px;
+  }
 </style>
